88140e7c990fe4d7555062ea8eaab776e59cfb22
root <root@DESKTOP-BMJLFU9.localdomain>
2023-09-23 22:01:00 +0100

    snapping alignment and effect; not yet fixing

--- Assets/MagneticSnapper.cs
+++ Assets/MagneticSnapper.cs
@@ -9,6 +9,8 @@ public class MagneticSnapper : MonoBehaviour
     public GameObject shadowBlock;
     private Material defaultMaterial;
     private Renderer meshRenderer;
+    private GameObject shadowBlockMagnetAlignmentHandle;
+    private Transform oppositeMagnetTransform = null;
 
 
     // Start is called before the first frame update
@@ -16,13 +18,17 @@ public class MagneticSnapper : MonoBehaviour
     {
         meshRenderer = transform.parent.gameObject.GetComponent<Renderer>();
         defaultMaterial = meshRenderer.material;
+        shadowBlockMagnetAlignmentHandle = shadowBlock.transform.parent.gameObject;
         Debug.Log("Default material: " + defaultMaterial);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (oppositeMagnetTransform != null)
+        {
+            MoveMagnetAlignmentHandleToFaceMagnet(oppositeMagnetTransform);
+        }
     }
 
     void OnTriggerEnter(Collider collider)
@@ -33,6 +39,9 @@ public class MagneticSnapper : MonoBehaviour
             var signedAngle = Vector3.SignedAngle(transform.forward, collider.transform.forward, Vector3.up);
             Debug.Log("Signed angle: " + signedAngle);
             if (Math.Abs(signedAngle) > 165f) {
+                oppositeMagnetTransform = collider.gameObject.transform;
+                transform.parent.gameObject.GetComponent<Renderer>().enabled = false;
+                shadowBlock.GetComponent<Renderer>().enabled = true;
                 SetMaterial(snappingMaterial);
             }
         }
@@ -43,8 +52,11 @@ public class MagneticSnapper : MonoBehaviour
         Debug.Log("Exited trigger");
         if (gameObject.GetInstanceID() > collider.gameObject.GetInstanceID()) 
         {
-            Debug.Log("I am the greatest");
-            SetMaterial(defaultMaterial);        
+            Debug.Log("I am still the greatest, but am leaving now...");
+            oppositeMagnetTransform = null; // NB could theoretically get multiple collisions... but not if blocks and magnets physically prevent it
+            transform.parent.gameObject.GetComponent<Renderer>().enabled = true;
+            shadowBlock.GetComponent<Renderer>().enabled = false;
+            SetMaterial(defaultMaterial);
         }
     }
 
@@ -53,5 +65,17 @@ public class MagneticSnapper : MonoBehaviour
         var materialsCopy = meshRenderer.materials;
         materialsCopy[0] = material;
         meshRenderer.materials = materialsCopy;
+    }
+
+    private void MoveMagnetAlignmentHandleToFaceMagnet(Transform magnetTransform)
+    {
+        shadowBlockMagnetAlignmentHandle.transform.position = magnetTransform.position;
+        RotateTransformToFaceReferenceTransform(shadowBlockMagnetAlignmentHandle.transform, magnetTransform);
+    }
+
+    private void RotateTransformToFaceReferenceTransform(Transform transform, Transform referenceTransform)
+    {
+        transform.rotation = referenceTransform.rotation;
+        transform.Rotate(0, 180f, 0);
     }
 }
