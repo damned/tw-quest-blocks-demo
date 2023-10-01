cd0b146214b7a540b656b3fe4a906d25cf7531e3
Dan Moore <dmoore@thoughtworks.com>
2023-10-01 23:01:14 +0100

    identified grabbed and released blocks in compound

--- Assets/Scripts/KinematicsCompound.cs
+++ Assets/Scripts/KinematicsCompound.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Transformers;
 
 public class KinematicsCompound : MonoBehaviour
 {
+    private const float TOLERANCE = 0.01f;
+    private Dictionary<GameObject, GameObject> controllerToGrabbedBlock = new Dictionary<GameObject, GameObject>();
 
     void Start()
     {
@@ -48,31 +51,54 @@ public class KinematicsCompound : MonoBehaviour
     public void AddBlock(GameObject block)
     {
         BindIntoCompound(block, transform);
+
     }
 
     public void OnGrab(SelectEnterEventArgs enterEvent)
     {
-        Pose attachPose = enterEvent.interactableObject.GetAttachPoseOnSelect(enterEvent.interactorObject);
-        Debug.Log("on compound grab, attach pose: " + attachPose);
-        int count = transform.childCount;
-        Debug.Log("compound child count: " + count);
-        for(int i = 0; i < count; i++)
+        var interactor = enterEvent.interactorObject;
+        var controller = ControllerOf(interactor);
+        Pose attachPose = enterEvent.interactableObject.GetAttachPoseOnSelect(interactor);
+        Debug.Log("grab: on compound grab with [" + controller.name +  "] attach pose: " + attachPose);
+        
+        for (int i = 0; i < transform.childCount; i++)
         {
             Transform blockTransform = transform.GetChild(i);
             var block = blockTransform.gameObject;
             var collider = block.GetComponent<Collider>();
 
-            var closestPoint = collider == null ? "n/a" : "" + collider.ClosestPoint(attachPose.position);
-            Debug.Log("for ref, child / block " + block.name + 
-                ", transform: " + blockTransform.position + 
-                ", distance: " + Vector3.Distance(blockTransform.position, attachPose.position) +
-                ", closest point on collider: " + closestPoint);
+            if (collider != null && Vector3.Distance(attachPose.position, collider.ClosestPoint(attachPose.position)) < TOLERANCE)
+            {
+                Debug.Log("grab: Found grabbed block: " + block.name);
+                if (controllerToGrabbedBlock.ContainsKey(controller))
+                {
+                    Debug.LogWarning("compound grab for controller [" + controller.name + "] already has a grabbed block assigned against it: " + controllerToGrabbedBlock[controller].name);
+                }
+                controllerToGrabbedBlock[controller] = block;
+            }
         }
     }
 
     public void OnRelease(SelectExitEventArgs exitEvent)
     {
-        Debug.Log("on compound release");
+        var interactor = exitEvent.interactorObject;
+        var controller = ControllerOf(interactor);
+        Debug.Log("release: on compound release with [" + controller.name +  "]");
+        if (controllerToGrabbedBlock.ContainsKey(controller))
+        {
+            var releasedBlock = controllerToGrabbedBlock[controller];
+            controllerToGrabbedBlock.Remove(controller);
+            Debug.Log("release: Found released block: " + releasedBlock);
+        }
+        else
+        {
+            Debug.LogWarning("compound release for controller [" + controller.name + "] has no grabbed block associated with it");
+        }
+    }
+
+    private static GameObject ControllerOf(IXRSelectInteractor interactor)
+    {
+        return interactor.transform.parent.gameObject;
     }
 
     private static void BindIntoCompound(GameObject block, Transform compoundTransform)
